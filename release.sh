e18b0b18530d6aba0c4b6f701cc5cf7e5933150e
Rainer Hihn <hihn.rainer@gmail.com>
2019-05-14 21:06:32 +0200

    M pom.xml  M release.sh

--- release.sh
+++ release.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 # rm -rf ~/.m2/repository/
+
 touch dependency-reduced-pom.xml pom.xml.releaseBackup release.properties
 rm dependency-reduced-pom.xml pom.xml.releaseBackup release.properties
 
-tmp='.tmp.commit'
-
-git pull github
-git status --porcelain -uno > $tmp
-git commit -a -F $tmp
-git push github
-rm $tmp
-
-
+/home/rainer/scripts/lazy-git.sh
 
 mvn clean -q
 # mvn deploy -X
-mvn release:prepare -Possrh
-mvn release:perform -Possrh
+mvn release:prepare -Phihn-releases
+mvn release:perform -Phihn-releases
